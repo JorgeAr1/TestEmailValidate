459c96816dfb206c96f5bc2f889ed62c54937698
Jorge Luis Arreygue Avila <coke_1224@hotmail.com>
2019-03-08 18:02:47 -0600

    Add all

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        [DataRow("jlarreygue@sciodev.com", true)]
+        [DataRow("jlarreygue@sciodev", true)]
+        [DataRow("jlarreygue@s@ciodev.com", false)]
+        [DataRow("@sciodev.com", false)]
+        [DataRow("jlarreygue@sciodev.com.com", true)]
+        [DataRow("jlarreyguesciodev.com", false)]
+        [DataRow("jlarreygue@", false)]
+        [DataRow("jlarrey_gue@sciodev.com", true)]
+        [DataRow("jlarreygue@scio_dev.com", true)]
+        public void TestMethod1(String email, bool res)
+        {
+            ValidateMail val = new ValidateMail();
+            Assert.AreEqual(val.validateMail(email), res);
+
+        }
+    }
+}
---
+++ UnitTestProject1/ValidateMail.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UnitTestProject1
+{
+    class ValidateMail
+    {
+        public bool validateMail(String email)
+        {
+            String expresionRegular = @"^[a-zA-Z0-9_\-\.]{2,}@[a-zA-Z0-9_\-\.]{2,}$";
+            Regex regex = new Regex(expresionRegular);
+            return regex.IsMatch(email);
+        }
+    }
+}
